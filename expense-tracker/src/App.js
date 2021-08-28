22ef717ea8611b129852357393ad7c057ade6866
Claudimar <claudimards@gmail.com>
2021-08-28 13:14:00 -0300

    seçao - react basics and working with components

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,11 @@
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Expenses />
+    </div>
+  );
+};
+
+export default App;
---
+++ expense-tracker/src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,21 @@
+import Card from '../Ui/Card'
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+
+const ExpenseItem = ({ date, title, amount }) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate
+        date={date}
+      />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">
+          ${amount.toFixed(2)}
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+export default ExpenseItem
